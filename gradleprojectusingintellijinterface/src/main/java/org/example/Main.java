fce7d314302adb8d817e059b7f19992d1d8b0d92
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-05-23 20:45:57 +0530

    Add files via upload

--- gradleprojectusingintellijinterface/src/main/java/org/example/Main.java
+++ gradleprojectusingintellijinterface/src/main/java/org/example/Main.java
@@ -6,22 +6,24 @@ import okhttp3.Response;
 
 public class Main {
     public static void main(String[] args) {
-
-       OkHttpClient client=new OkHttpClient();//it is library used to make http calls easily
-
-        String url="https://jsonplaceholder.typicode.com/todos/1";
-        Request request=new Request.Builder()
-                .url(url)
-                .build();
-        try (Response response=client.newCall(request).execute()){
-            if(!response.isSuccessful()){
-                System.out.println("Something went wrong!! ");
-            }
-            System.out.println(response.body().string());
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        System.out.println("Hello");
+//       OkHttpClient client=new OkHttpClient();//it is library used to make http calls easily
+//
+//        String url="https://jsonplaceholder.typicode.com/todos/1";
+//        Request request=new Request.Builder()
+//                .url(url)
+//                .build();
+//        try (Response response=client.newCall(request).execute()){// client.newcall(request).execute() makes tcp connection between server and user
+//            if(!response.isSuccessful()){
+//                System.out.println("Something went wrong!! ");
+//            }
+//            System.out.println(response.body().string());
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
+//Retrofit retrofit=new Retrofit.Builder()
+//        .build(url)
 
     }
 }
